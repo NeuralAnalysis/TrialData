25a7c5017130dcc9f05a34a3fb418caee2e5dab5
mattperich <mperich@gmail.com>
2017-02-23 14:48:27 -0600

    Merged getGLM and getLinModel into getModel.m

--- Analysis/getModel.m
+++ Analysis/getModel.m
@@ -1,20 +1,27 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function [trial_data,glm_info] = getGLM(trial_data,params)
+% function [trial_data,glm_info] = getGLM(trial_data,model_type,params)
 %
-%   This function will fit a GLM to take any combination of inputs and
-% predict some outputs. Actually has two modes:
+%   This function will fit a linear model to take any combination of inputs
+% and predict some outputs. Supports GLM or normal linear models. Doesn't
+% incorporate history outright. If you want history you should use
+% dupeAndShift or convBasisFunc.
 %
-%   [trial_data,glm_info] = getGLM(trial_data,params);
-%       This mode will fit a GLM and return the struct with GLM predictions
+% Actually has two modes:
+%
+%   [trial_data,model_info] = getModel(trial_data,model_type,params);
+%       This mode will fit a model and return the struct with GLM predictions
 %       added as a field, as well as GLM parameters.
 %
-%   trial_data            = getGLM(trial_data,glm_info)
-%       This mode will take the glm_info from a previous getGLM call and
-%       add GLM predictions as a field to trial_data (e.g. for new trials)
+%   trial_data            = getModel(trial_data,model_type,model_info)
+%       This mode will take the model_info from a previous getModel call and
+%       add model predictions as a field to trial_data (e.g. for new trials)
 %
 %
 % INPUTS:
 %   trial_data : the struct
+%   model_type : (string) which model to use
+%       1) 'linmodel' : a simple linear filter
+%       2) 'glm'    : a generalized linear model
 %   in_struct  : a struct of inputs. Can be one of two things:
 %       1) glm_info   : struct of model fit info from getGLM call (for predicting)
 %       2) params     : parameter struct (NOTE: must NOT have 'b' or 's' fields)
@@ -22,12 +29,16 @@
 %            .in_signals   : (cell) GLM inputs in form {'name',idx; 'name',idx};
 %            .out_signals  : (cell) GLM outputs in form {'name',idx}
 %            .train_idx    : trial indices for training (will predict for all)
+%          GLM-specific parameters:
 %            .do_lasso     : flag to use lasso regularization (default: false)
 %            .lasso_lambda : lambda parameter for lasso
 %            .lasso_alpha  : alpha parameter for lasso
 %
 % Note: at the moment, only supports one output field. But can have any
 %       arbitrary number of input fields, which it will stick together
+%
+% To do:
+%   1) Implement something like ridge regression for linmodel
 %
 % EXAMPLES:
 %   e.g. to fit a GLM
@@ -38,23 +49,23 @@
 % Written by Matt Perich. Updated Feb 2017.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [trial_data,glm_info] = getGLM(trial_data,params)
+function [trial_data,model_info] = getModel(trial_data,model_type,params)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DEFAULT PARAMETERS
 model_name    =  'default';
 in_signals    =  {};%{'name',idx; 'name',idx};
 out_signals   =  {};%{'name',idx};
 train_idx     =  1:length(trial_data);
+% GLM-specific parameters
 do_lasso      =  false;
 lasso_lambda  =  0;
 lasso_alpha   =  0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Here are some parameters that you can overwrite that aren't documented
-glm_distribution     =  'poisson'; % which distribution to assume for GLM
-add_pred_to_td       =  true;      % whether to add predictions to trial_data
-td_fieldname_prefix  =  'glm';     % name prefix for trial_data field
-b                    =  [];        % b and s are to identify if glm_info
-s                    =  [];        %    was provided as a params input
+add_pred_to_td       =  true;        % whether to add predictions to trial_data
+glm_distribution     =  'poisson';   % which distribution to assume for GLM
+td_fieldname_prefix  =  model_type;  % name prefix for trial_data field
+b                    =  [];          % b identifies if model_info was provided as a params input
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 assignParams(who,params); % overwrite parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -63,7 +74,7 @@ if isempty(in_signals) || isempty(out_signals) || ~iscell(in_signals) || ~iscell
     error('input/output info must be provided in cells');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if isempty(b) && isempty(s) % fit a new GLM
+if isempty(b)  % fit a new model
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % build inputs and outputs for training
     x = get_vars(trial_data(train_idx),in_signals);
@@ -73,14 +84,19 @@ if isempty(b) && isempty(s) % fit a new GLM
     % Fit GLMs
     b = zeros(size(x,2)+1,size(y,2));
     for iVar = 1:size(y,2) % loop along outputs to predict
-        if do_lasso % not quite implemented yet
-            [b,temp] = lassoglm(x,y(:,iVar),glm_distribution,'lambda',lasso_lambda,'alpha',lasso_alpha);
-            b = [temp.Intercept; b];
-        else
-            [b(:,iVar),~,temp] = glmfit(x,y(:,iVar),glm_distribution);
+        switch lower(model_type)
+            case 'glm'
+                if do_lasso % not quite implemented yet
+                    [b,temp] = lassoglm(x,y(:,iVar),glm_distribution,'lambda',lasso_lambda,'alpha',lasso_alpha);
+                    b = [temp.Intercept; b];
+                else
+                    [b(:,iVar),~,temp] = glmfit(x,y(:,iVar),glm_distribution);
+                end
+                if isempty(s), s = temp; end
+                s(iVar) = temp;
+            case 'linmodel'
+                b(:,iVar) = [ones(size(x,1),1), x]\y(:,iVar);
         end
-        if isempty(s), s = temp; end
-        s(iVar) = temp;
     end
 else % use an old GLM
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -97,7 +113,12 @@ if add_pred_to_td
         x  = get_vars(trial_data(trial),in_signals);
         yfit = zeros(size(x,1),size(b,2));
         for iVar = 1:size(b,2)
-            yfit(:,iVar) = exp([ones(size(x,1),1), x]*b(:,iVar));
+            switch lower(model_type)
+                case 'glm'
+                    yfit(:,iVar) = exp([ones(size(x,1),1), x]*b(:,iVar));
+                case 'linmodel'
+                    yfit(:,iVar) = [ones(size(in_data,1),1), in_data]*b(:,iVar);
+            end
         end
         trial_data(trial).([td_fieldname_prefix '_' model_name]) = yfit;
     end
@@ -105,18 +126,28 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Package up outputs
-s = rmfield(s,{'resid','residp','residd','resida','wts'});
-glm_info = struct( ...
-    'model_name',   model_name, ...
-    'in_signals',   {in_signals}, ...
-    'out_signals',  {out_signals}, ...
-    'train_idx',    train_idx, ...
-    'b',            b, ...
-    's',            s, ...
-    'do_lasso',     do_lasso, ...
-    'lasso_lambda', lasso_lambda, ...
-    'lasso_alpha',  lasso_alpha);
-
+switch lower(model_type)
+    case 'glm'
+        s = rmfield(s,{'resid','residp','residd','resida','wts'});
+        model_info = struct( ...
+            'model_name',   model_name, ...
+            'in_signals',   {in_signals}, ...
+            'out_signals',  {out_signals}, ...
+            'train_idx',    train_idx, ...
+            'b',            b, ...
+            's',            s, ...
+            'do_lasso',     do_lasso, ...
+            'lasso_lambda', lasso_lambda, ...
+            'lasso_alpha',  lasso_alpha);
+        
+    case 'linmodel'
+        model_info = struct( ...
+            'model_name',   model_name, ...
+            'in_signals',   {in_signals}, ...
+            'out_signals',  {out_signals}, ...
+            'train_idx',    train_idx, ...
+            'b',            b);
+end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
