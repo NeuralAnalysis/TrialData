d30b53d5e737917ba2060be5c18acf4b88001859
mattperich <mperich@gmail.com>
2017-03-08 17:39:22 -0600

    Added moving average code

---
+++ util/moving_average.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function data_mvavg = moving_average(data, bins);
+%
+%   Computes moving average of input signal(s)
+%
+% INPUTS:
+%   data : the data to smooth (rows are time, columns are signals)
+%   bins : how many time bins to use for each side of the filter
+%             so, bins = 2 gives a moving average filter of total width = 5
+%
+% OUTPUTS:
+%   data_mvavg : smoothed data. Same size as data
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function data_mvavg = moving_average(data,bins)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+data_mvavg = zeros(size(data));
+for i = 1:size(data,2)
+    L = filter(ones(2*bins+1,1)/(2*bins+1),1,[data(:,i); zeros(bins,1)]);
+    data_mvavg(:,i) = L(bins+1:end);
+end
