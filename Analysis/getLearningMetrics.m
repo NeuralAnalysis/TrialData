4f6c731c86d333237624d11212569ab7589b4376
mattperich <mperich@gmail.com>
2017-03-10 21:30:44 -0600

    Updates to getLearningMetrics

--- Analysis/getLearningMetrics.m
+++ Analysis/getLearningMetrics.m
@@ -10,31 +10,23 @@
 %                       'angle': angular takeoff error
 %                       'corr' : speed profile correlation coefficients
 %                       'time' : time to target
-%     .result_codes : which to include. Default is 'R', 'I'.
 %     .use_bl_ref   : (bool) whether to use diff from BL for angle
 %     .time_window  : {'idx_start',bins after; 'idx_end',bins after} currently for angle
 %     .corr_samples : how many datapoints to interpolate trajectory onto for corr
 %     .vel_or_pos   : 'vel' or 'pos', for velocity or position (angle only)
-% 
+%
 % Written by Matt Perich. Updated Feb 2017.
-% 
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function metric = getLearningMetrics(trial_data,params)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 which_metric = 'angle';
 time_window = {'idx_movement_on',0; 'idx_peak_speed',0};
 use_bl_ref = true;
-result_codes = {'R'};
 corr_samples = 1000;
 vel_or_pos = 'vel';
 if nargin > 1, assignParams(who,params); end % overwrite parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-if isfield(trial_data(1),'result')
-    trial_data = trial_data(ismember({trial_data.result},result_codes));
-else
-    disp('Result field not found...');
-end
 
 utheta = unique([trial_data.target_direction]);
 metric = zeros(length(trial_data),1);
@@ -51,54 +43,45 @@ switch lower(which_metric)
                     error('No BL ref found for all targets');
                 end
                 
-                temp = zeros(length(bl_idx),1);
+                temp_err = zeros(length(bl_idx),1);
                 for iTrial = 1:length(bl_idx)
                     t1 = trial_data(bl_idx(iTrial)).(time_window{1,1})+time_window{1,2};
                     t2 = trial_data(bl_idx(iTrial)).(time_window{2,1})+time_window{2,2};
-                    switch vel_or_pos
-                        case 'vel'
-                            temp(iTrial) = angleDiff(minusPi2Pi(trial_data(bl_idx(iTrial)).target_direction), ...
-                                atan2(trial_data(bl_idx(iTrial)).vel(t2,2) - ...
-                                trial_data(bl_idx(iTrial)).vel(t1,2), ...
-                                trial_data(bl_idx(iTrial)).vel(t2,1) - ...
-                                trial_data(bl_idx(iTrial)).vel(t1,1)), ...
-                                true,true);
-                        case 'pos'
-                            temp(iTrial) = angleDiff(minusPi2Pi(trial_data(bl_idx(iTrial)).target_direction), ...
-                                atan2(trial_data(bl_idx(iTrial)).pos(t2,2) - ...
-                                trial_data(bl_idx(iTrial)).pos(t1,2), ...
-                                trial_data(bl_idx(iTrial)).pos(t2,1) - ...
-                                trial_data(bl_idx(iTrial)).pos(t1,1)), ...
-                                true,true);
-                    end
+                    temp = trial_data(bl_idx(iTrial)).(vel_or_pos);
+                    temp_err(iTrial) = angleDiff(minusPi2Pi(trial_data(bl_idx(iTrial)).target_direction), ...
+                        atan2(temp(t2,2) - temp(t1,2), ...
+                        temp(t2,1) - temp(t1,1)), ...
+                        true,true);
                 end
-                bl_metric(iDir) = circular_mean(temp);
+                bl_metric(iDir) = mean(temp_err);
             end, clear temp;
+        end
+        if any(abs(bl_metric) > 1)
+            keyboard
         end
         
         % get velocity at time of peak speed
         for iTrial = 1:length(trial_data)
             t1 = trial_data(iTrial).(time_window{1,1})+time_window{1,2};
             t2 = trial_data(iTrial).(time_window{2,1})+time_window{2,2};
-            switch vel_or_pos
-                case 'vel'
-                    temp = angleDiff(trial_data(iTrial).target_direction, ...
-                        atan2(trial_data(iTrial).vel(t2,2) - ...
-                        trial_data(iTrial).vel(t1,2), ...
-                        trial_data(iTrial).vel(t2,1) - ...
-                        trial_data(iTrial).vel(t1,1)), ...
-                        true,true);
-                case 'pos'
-                    temp = angleDiff(trial_data(iTrial).target_direction, ...
-                        atan2(trial_data(iTrial).pos(t2,2) - ...
-                        trial_data(iTrial).pos(t1,2), ...
-                        trial_data(iTrial).pos(t2,1) - ...
-                        trial_data(iTrial).pos(t1,1)), ...
-                        true,true);
-            end
+            
+            temp = trial_data(iTrial).(vel_or_pos);
+            temp_err = angleDiff(minusPi2Pi(trial_data(iTrial).target_direction), ...
+                atan2(temp(t2,2) - temp(t1,2), ...
+                temp(t2,1) - temp(t1,1)), ...
+                true,true);
+            
             iDir = utheta==trial_data(iTrial).target_direction;
-            metric(iTrial) = angleDiff(bl_metric(iDir),temp,true,true);
+            metric(iTrial) = angleDiff(bl_metric(iDir),temp_err,true,true);
+            if metric(iTrial) > pi/2
+                warning('Hack this shit until you figure out why the CDS had the wrong words.');
+                metric(iTrial) = metric(iTrial)-pi;
+            elseif metric(iTrial) < -pi/2
+                warning('Hack this shit until you figure out why the CDS had the wrong words.');
+                metric(iTrial) = metric(iTrial)+pi;
+            end
         end, clear temp;
+        
         
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -118,8 +101,7 @@ switch lower(which_metric)
                 
                 idx = t1:t2;
                 temp = trial_data(bl_idx(iTrial)).vel;
-                %                 temp(:,1) = smoothSpikesForPCA(temp(:,1),1,10);
-                %                 temp(:,2) = smoothSpikesForPCA(temp(:,2),1,10);
+                
                 bl_temp(iTrial,1,:) = interp1(1:length(idx),temp(idx,1),linspace(1,length(idx),corr_samples));
                 bl_temp(iTrial,2,:) = interp1(1:length(idx),temp(idx,2),linspace(1,length(idx),corr_samples));
             end
@@ -134,10 +116,7 @@ switch lower(which_metric)
             iDir = utheta==trial_data(iTrial).target_direction;
             
             temp = trial_data(iTrial).vel;
-%             if strcmpi(trial_data(iTrial).epoch,'ad')
-%                 temp(:,1) = smoothSpikesForPCA(temp(:,1),1,10);
-%                 temp(:,2) = smoothSpikesForPCA(temp(:,2),1,10);
-%             end
+            
             temp = [interp1(1:length(idx),temp(idx,1),linspace(1,length(idx),corr_samples))', ...
                 interp1(1:length(idx),temp(idx,2),linspace(1,length(idx),corr_samples))'];
             metric(iTrial) = corr2(squeeze(bl_metric(iDir,:,:)),temp)^2;
--- util/moving_average.m
+++ util/moving_average.m
@@ -20,3 +20,10 @@ for i = 1:size(data,2)
     L = filter(ones(2*bins+1,1)/(2*bins+1),1,[data(:,i); zeros(bins,1)]);
     data_mvavg(:,i) = L(bins+1:end);
 end
+
+% data_mvavg = zeros(size(data,1)-(bins),size(data,2));
+% for i = 1:size(data,2)
+%     for j = 1:size(data,1)-(bins)
+%         data_mvavg(j,i) = mean(data(j:j+bins,i));
+%     end
+% end
