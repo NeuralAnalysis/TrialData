45f0c527bf1d71e9fbda015e85460149c8dd461e
mattperich <mperich@gmail.com>
2017-03-12 01:03:53 -0600

    Added some comments to rel pseudo r2

--- util/compute_rel_pseudo_R2.m
+++ util/compute_rel_pseudo_R2.m
@@ -1,5 +1,7 @@
 function R2 = compute_rel_pseudo_R2(Y, Yhat1, Yhat2)
-
+% Y     : original signal (real, predicted data)
+% Yhat1 : the reduced model to compare against
+% Yhat2 : the full model to compare against
 if(nargin < 4)
   Model1r = sum(Y.*log(eps+Yhat1) - Yhat1);
   Model2r = sum(Y.*log(eps+Yhat2) - Yhat2);
