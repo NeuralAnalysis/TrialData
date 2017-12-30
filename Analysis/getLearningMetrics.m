c7a584090f3e60ce0267f7c6f4eb32338212f031
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2017-12-29 20:50:15 -0600

    add curve fitting for baseline learning metrics

--- Analysis/getLearningMetrics.m
+++ Analysis/getLearningMetrics.m
@@ -23,6 +23,7 @@ function metric = getLearningMetrics(trial_data,params)
 which_metric = 'angle';
 time_window = {'idx_movement_on',0; 'idx_peak_speed',0};
 use_bl_ref = true;
+fit_bl_ref_curve = false;
 corr_samples = 1000;
 vel_or_pos = 'vel';
 if nargin > 1, assignParams(who,params); end % overwrite parameters
@@ -37,10 +38,15 @@ switch lower(which_metric)
         % get baseline error to each target
         bl_metric = zeros(length(utheta),1);
         if use_bl_ref
-            for iDir = 1:length(utheta)
-                bl_idx = getTDidx(trial_data,'epoch','bl','target_direction',utheta(iDir));
+            if fit_bl_ref_curve
+                % try fitting sins and cosines for bl_ref
+                num_freqs = 3;
+                [bl_idx,bl_trials] = getTDidx(trial_data,'epoch','bl');
+                theta = [bl_trials.target_direction];
+                sincos = [sin(theta'*(1:num_freqs)) cos(theta'*(1:num_freqs))];
+                usincos = [sin(utheta'*(1:num_freqs)) cos(utheta'*(1:num_freqs))];
                 if isempty(bl_idx)
-                    error('No BL ref found for all targets');
+                    error('No BL ref found');
                 end
                 
                 temp_err = zeros(length(bl_idx),1);
@@ -52,8 +58,37 @@ switch lower(which_metric)
                         atan2(temp(t2,2) - temp(t1,2), temp(t2,1) - temp(t1,1)), ...
                         true,true);
                 end
-                bl_metric(iDir) = mean(temp_err);
-            end, clear temp;
+
+                % fit function
+                lm_bl = fitlm(sincos,temp_err);
+                bl_metric = lm_bl.predict(usincos);
+            % end
+            % if true
+            else
+                for iDir = 1:length(utheta)
+                    bl_idx = getTDidx(trial_data,'epoch','bl','target_direction',utheta(iDir));
+                    if isempty(bl_idx)
+                        error('No BL ref found for all targets');
+                    end
+                    
+                    temp_err = zeros(length(bl_idx),1);
+                    for iTrial = 1:length(bl_idx)
+                        t1 = trial_data(bl_idx(iTrial)).(time_window{1,1})+time_window{1,2};
+                        t2 = trial_data(bl_idx(iTrial)).(time_window{2,1})+time_window{2,2};
+                        temp = trial_data(bl_idx(iTrial)).(vel_or_pos);
+                        temp_err(iTrial) = angleDiff(minusPi2Pi(trial_data(bl_idx(iTrial)).target_direction), ...
+                            atan2(temp(t2,2) - temp(t1,2), temp(t2,1) - temp(t1,1)), ...
+                            true,true);
+                    end
+                    bl_metric(iDir) = mean(temp_err);
+                end, clear temp;
+                % figure; plot(utheta,bl_metric','o');
+                % theta = linspace(0,2*pi,100);
+                % sincos = [sin(theta'*(1:num_freqs)) cos(theta'*(1:num_freqs))];
+                % blspline = lm_bl.predict(sincos);
+                % hold on
+                % plot(theta,blspline)
+            end
         end
         
         % get velocity at time of peak speed
