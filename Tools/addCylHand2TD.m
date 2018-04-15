06ced9fd7c198f4ed82187178431efb146732fb2
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2018-04-14 21:22:24 -0500

    add new function to add cylindrical coordinates of hand to td

--- Tools/addCylHand2TD.m
+++ Tools/addCylHand2TD.m
@@ -1,27 +1,27 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function trial_data = addSphereHand2TD(trial_data)
+% function trial_data = addCylHand2TD(trial_data)
 %
-%   Adds spherical coordinates for opensim hand position to trial_data.
+%   Adds cylindrical coordinates for opensim hand position to trial_data.
 %   New signals are called:
-%   'sphere_hand_pos'
-%   'sphere_hand_vel'
-%   'sphere_hand_acc'
+%   'cyl_hand_pos'
+%   'cyl_hand_vel'
+%   'cyl_hand_acc'
 %   Order of coordinates in new signals is: azimuth, elevation, r
 %
 % INPUTS:
 %   trial_data : the struct
 %
 % OUTPUTS:
-%   trial_data : the struct with .sphere_hand_* fields (reordered logically)
+%   trial_data : the struct with .cyl_hand_* fields (reordered logically)
 %
 % Written by Raeed Chowdhury. Updated Jan 2018.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function trial_data = addSphereHand2TD(trial_data)
+function trial_data = addCylHand2TD(trial_data)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cart_prefix = {'X','Y','Z'};
 cart_postfix = {'_handPos','_handVel','_handAcc'};
-sphere_postfix = {'pos','vel','acc'};
+cyl_postfix = {'pos','vel','acc'};
 for trial = 1:length(trial_data)
     % check for opensim field
     if isfield(trial_data(trial),'opensim') && isfield(trial_data(trial),'opensim_names')
@@ -35,10 +35,10 @@ for trial = 1:length(trial_data)
                     coords = [coords trial_data(trial).opensim(:,contains(trial_data(trial).opensim_names,...
                                                                         [cart_prefix{prefix_ctr} cart_postfix{postfix_ctr}]))];
                 end
-                [az,el,r] = cart2sph(coords(:,1),coords(:,2),coords(:,3));
-                trial_data(trial).(['sphere_hand_' sphere_postfix{postfix_ctr}]) = [az el r];
+                [az,el,r] = cart2pol(coords(:,1),coords(:,2),coords(:,3));
+                trial_data(trial).(['cyl_hand_' cyl_postfix{postfix_ctr}]) = [az el r];
             else
-                warning(['no hand marker ' sphere_postfix{postfix_ctr} ' found for one or more trials'])
+                warning(['no hand marker ' cyl_postfix{postfix_ctr} ' found for one or more trials'])
             end
         end
     else
@@ -48,3 +48,4 @@ end
 
 % restore logical order
 trial_data = reorderTDfields(trial_data);
+
