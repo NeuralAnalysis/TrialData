656c8c9500321e12a2b465c6f1606ab26d12bd0a
mattperich <mperich@gmail.com>
2018-07-09 08:23:01 +0200

    Added documentation to stripSpikeSorting

--- Tools/stripSpikeSorting.m
+++ Tools/stripSpikeSorting.m
@@ -1,8 +1,18 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function trial_data = stripSpikeSorting(trial_data)
+%
+%   trial_data = stripSpikeSorting(trial_data)
+%   This function, as the name implies, strips away the sort codes for all
+% of the spiking arrays. Essentially, summing all of the spike counts in
+% each bin for all units identified on a single electrode.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function trial_data = stripSpikeSorting(trial_data)
-% as the name implies, this strips away the spike sorting
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% get the arrays that are present
 fn_array = getTDfields(trial_data,'arrays');
 
+% loop along the arrays
 for iArray = 1:length(fn_array)
     sg = trial_data(1).([fn_array{iArray} '_unit_guide']);
     elecs = unique(sg(:,1));
@@ -12,11 +22,13 @@ for iArray = 1:length(fn_array)
         trial_data(trial).temp_spikes = NaN(size(trial_data(trial).([fn_array{iArray} '_spikes']),1),length(elecs));
     end
     
+    % sum the spikes for all units on each electrode
     for e = 1:length(elecs)
         idx = sg(:,1) == elecs(e);
         
         for trial = 1:length(trial_data)
             temp = trial_data(trial).([fn_array{iArray} '_spikes']);
+            % store result in the temporary field
             trial_data(trial).temp_spikes(:,e) = sum(temp(:,idx),2);
         end
     end
@@ -28,5 +40,6 @@ for iArray = 1:length(fn_array)
         trial_data(trial).([fn_array{iArray} '_spikes']) = trial_data(trial).temp_spikes;
         trial_data(trial).([fn_array{iArray} '_unit_guide']) = sg_new;
     end
+    % get rid of this useless old thing
     trial_data = rmfield(trial_data,'temp_spikes');
 end
