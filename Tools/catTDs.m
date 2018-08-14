36f7bb349dc47a4026e18307e9ba938164161d3e
mattperich <mperich@gmail.com>
2018-08-14 16:10:30 +0200

    Bug fix in catTDs

--- Tools/catTDs.m
+++ Tools/catTDs.m
@@ -42,33 +42,32 @@ if length(varargin) > 1
     for iTD = 1:length(varargin)
         
         if isempty(master_td)
-            master_td = varargin{iTD};
+            fn_master = fieldnames(varargin{iTD});
+        else
+            fn_master = fieldnames(master_td);
         end
         
-        if ~isempty(master_td)
-            td = varargin{iTD};
-            
-            fn_master = fieldnames(master_td);
-            fn_new = fieldnames(td);
-            
-            % see what is missing from the master, and populate it with NaN
-            fn_miss = setdiff(fn_new,fn_master);
-            for i = 1:length(fn_miss)
-                for trial = 1:length(master_td)
-                    master_td(trial).(fn_miss{i}) = NaN;
-                end
+        td = varargin{iTD};
+        
+        fn_new = fieldnames(td);
+        
+        % see what is missing from the master, and populate it with NaN
+        fn_miss = setdiff(fn_new,fn_master);
+        for i = 1:length(fn_miss)
+            for trial = 1:length(master_td)
+                master_td(trial).(fn_miss{i}) = NaN;
             end
-            
-            % see what is missing from the new, and populate it with NaN
-            fn_miss = setdiff(fn_master,fn_new);
-            for i = 1:length(fn_miss)
-                for trial = 1:length(td)
-                    td(trial).(fn_miss{i}) = NaN;
-                end
-            end
-            
-            master_td = [master_td,td];
         end
+        
+        % see what is missing from the new, and populate it with NaN
+        fn_miss = setdiff(fn_master,fn_new);
+        for i = 1:length(fn_miss)
+            for trial = 1:length(td)
+                td(trial).(fn_miss{i}) = NaN;
+            end
+        end
+        
+        master_td = [master_td,td];
     end
 else
     disp('Only one trial_data struct. Nothing to concatenate...');
