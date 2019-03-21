ae7483e3ada12909964ee2231dd62abd02cc1c64
mattperich <mperich@gmail.com>
2019-03-21 19:58:45 +0100

    Updated getLFPidx to be more flexible for inputs

--- Tools/getLFPidx.m
+++ Tools/getLFPidx.m
@@ -1,9 +1,9 @@
-function [idx,freqs,chans] = getLFPidx(td,params)
+function [idx,freqs,chans] = getLFPidx(td,varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
 % function [idx,freqs,chans] = getLFPidx(td, chans, freqs)
 %
-%   This function 
+%   This function will query the lfp_guide and return indices.
 % 
 % INPUTS:
 %   td               :  the struct
@@ -12,6 +12,10 @@ function [idx,freqs,chans] = getLFPidx(td,params)
 %       .channels    :  which channels to return. Use [] or 'all' for all
 %       .freq_bands  :  which frequency ranges [LOW, HIGH]
 %                           use [] or 'all' for all
+%
+%   OR
+%
+%   Instead of a params struct, ...'NAME',VALUE... pairs
 %
 % OUTPUTS:
 %   idx       :  
@@ -26,16 +30,41 @@ channels    =  'all';
 freq_bands  =  'all';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin > 1
-    assignParams(who,params);
+    if nargin == 2 % it's a params struct
+        params = varargin{1};
+        if isstruct(params)
+            assignParams(who,params);
+        else
+            error('Could not parse input to getLFPidx. Should either be a params struct or ...''NAME'',VALUE... pairs.');
+        end
+    else % it's name value pairs
+        if mod(nargin,2) ~= 0 % make sure there are pairs
+            error('Could not parse input to getLFPidx. Should either be a params struct or ...''NAME'',VALUE... pairs.');
+        else
+            params = struct();
+            for iVar = 1:2:length(varargin)
+                params.(varargin{iVar})  =  varargin{iVar+1};
+            end
+            assignParams(who,params);
+        end
+    end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if isempty(array)
     % first check if there is more than one LFP
     fn = getTDfields(td,'lfp');
-    if isempty(fn)
-        error('No LFP field found');
+    if isempty(fn) % no LFP, no need for this function!
+        disp('ERROR: getTDfields: No LFP field found!');
+        idx = [];
+        freqs = [];
+        chans = [];
+        return;
     elseif length(fn) > 1
-        error('Multiple  LFP fields found...  you need to specify which array.');
+        disp('ERROR: getTDfields: Multiple  LFP fields found...  you need to specify which array.');
+        idx = [];
+        freqs = [];
+        chans = [];
+        return;
     elseif length(fn) == 1
         array = getTDfields(td,'arrays');
     end
