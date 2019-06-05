c1b5ec16d64c12a47c3d3c1c45c36de5fd2f4867
timpx <timpx@mm.st>
2019-06-05 16:50:25 +0200

    correct bug to allow using idx_values after performing appendTDs

--- Tools/appendTDs.m
+++ Tools/appendTDs.m
@@ -70,17 +70,18 @@ if length(varargin) > 1
         for trial = 1:length(combined_td)
             combined_td(trial).stitch_marks = [combined_td(trial).stitch_marks, size(combined_td(trial).(fn_time{1}),1)];
             
+            % adjust the idx_values keeping only the last one
+            for var = 1:length(fn_idx)
+                if ~isempty(td(trial).(fn_idx{var})) && sum(~isnan(td(trial).(fn_idx{var})))
+                   combined_td(trial).(fn_idx{var}) = td(trial).(fn_idx{var})+size(combined_td(trial).(fn_time{1}),1);
+                end
+            end
+
             % append the time variables
             for var = 1:length(fn_time)
                 combined_td(trial).(fn_time{var}) = [combined_td(trial).(fn_time{var}); td(trial).(fn_time{var})];
             end
             
-            % adjust the idx_values
-            for var = 1:length(fn_idx)
-                if ~isempty(td(trial).(fn_idx{var}))
-                    combined_td(trial).(fn_idx{var}) = td(trial).(fn_idx{var})+size(combined_td(trial).(fn_time{1}),1);
-                end
-            end
         end
     end
 else
