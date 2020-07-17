0624ef6b3ec9890fece69f19aa123fa56c40a352
Matt Perich <mattperich@Matts-MBP.home>
2020-07-17 13:33:58 -0400

    Updated getTDfields to handle cases where the time fields on some trials might be NaN, as a result of concatenating structs with different fields. So now it looks over all trials to find any possible time fields and then finds the unique ones

--- Tools/getTDfields.m
+++ Tools/getTDfields.m
@@ -20,7 +20,7 @@
 % OUTPUTS:
 %   fn : the fieldnames of which_type
 %
-% Written by Matt Perich. Updated Feb 2017.
+% Written by Matt Perich. Updated July 2020.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function fn = getTDfields(trial_data,which_type,cont_var_ref)
@@ -63,20 +63,14 @@ switch lower(which_type)
         % getting zero or one
         t = 0;
         iVar = 0;
+        trial_lengths = zeros(1,length(trial_data));
         while t == 0 && iVar < length(cont_vars_here)
             iVar = iVar + 1;
-            [t,trial_idx] = max(cellfun(@(x) size(x,1),{trial_data.(cont_vars_here{iVar})}));
+            trial_lengths = nanmax( cat(1,trial_lengths,cellfun(@(x) size(x,1),{trial_data.(cont_vars_here{iVar})})) );
         end
+        [t,~] = nanmax( trial_lengths );
         if t == 0
             error('Time variables appear to have zero bins.');
-        elseif t > 1
-            idx = false(length(fn),1);
-            for ifn = 1:length(fn)
-                idx(ifn) = size(trial_data(trial_idx).(fn{ifn}),1)==t & ...
-                    isempty(regexp(fn{ifn},'_unit_guide', 'once')) & ...
-                    isempty(regexp(fn{ifn},'_lfp_guide', 'once'));
-            end
-            fn = fn(idx);
         elseif t == 1
             % there is an edge case where if there is only one time bin it
             % returns everything. So, try and eliminate things through
@@ -103,6 +97,26 @@ switch lower(which_type)
             end
             good_idx = good_idx | ismember(fn,fn_spikes);
             fn = fn(good_idx & ~bad_idx);
+            
+        elseif t > 1
+            % now this is the meat of it. Sometimes a time field can be
+            % populated by NaN if it's merged with catTDs and doesn't exist
+            % in all trials. So, loop along all trials where the time is
+            % greater than 1, find all the time signals so we are sure we
+            % don't miss anything in the edge case where the longest trial
+            % has a NaN for one of the time fields
+            idx = false(length(fn),1);
+            for trial = 1:length(trial_data)
+                if trial_lengths(trial) > 1
+                    for iSig = 1:length(fn)
+                        idx(iSig) = idx(iSig) | ...
+                            size(trial_data(trial).(fn{iSig}),1)==trial_lengths(trial) & ...
+                            isempty(regexp(fn{iSig},'_unit_guide', 'once')) & ...
+                            isempty(regexp(fn{iSig},'_lfp_guide', 'once'));
+                    end
+                end
+            end
+            fn = fn(idx);
         end
         
     case 'meta'% all fields that are NOT time-varying or idx_
